89dbc583b0fc84f7026692f34a3a6da6c0ee7bfb
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-05 15:00:17 -0300

    refactor: Atualiza repositórios

--- src/infra/db/memory/shortUrlRepository.ts
+++ src/infra/db/memory/shortUrlRepository.ts
@@ -5,19 +5,15 @@ export class ShortUrlRepository implements BaseRepository<ShortUrl> {
 	async save(entity: ShortUrl): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
-
 	async findMany(): Promise<ShortUrl[]> {
 		throw new Error('Method not implemented.');
 	}
-
-	async findOne(): Promise<ShortUrl> {
+	async findOne(identifier: string): Promise<ShortUrl | null> {
 		throw new Error('Method not implemented.');
 	}
-
 	async update(): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
-
 	async delete(): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
