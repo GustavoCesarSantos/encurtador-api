14b8addecff07dbd7dc779810538c675dde678f6
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-05 12:46:28 -0300

    refactor(shortUrl): Adiciona repositorio ao construtor do caso de uso de salvar

--- src/modules/shortUrls/useCases/saveShortUrl.ts
+++ src/modules/shortUrls/useCases/saveShortUrl.ts
@@ -1,10 +1,17 @@
 import { ShortUrl } from '@/shortUrls/shortUrl';
+import { BaseRepository } from 'infra/db/baseRepository';
 
 export interface ISaveShortUrl {
 	execute(url: string, code: string): Promise<void>;
 }
 
 export class SaveShortUrl implements ISaveShortUrl {
+	private readonly shortUrlRepository: BaseRepository<ShortUrl>;
+
+	constructor(shortUrlRepository: BaseRepository<ShortUrl>) {
+		this.shortUrlRepository = shortUrlRepository;
+	}
+
 	async execute(url: string, code: string): Promise<void> {
 		const shortUrl = new ShortUrl({
 			url,
