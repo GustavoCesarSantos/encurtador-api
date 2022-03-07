195aafe7a07a3892ffe716c10e8d1ae5681cdbaf
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-07 00:25:09 -0300

    feat: Cria caso de uso de deletar url encurtada

---
+++ src/modules/shortUrls/useCases/deleteShortUrl.ts
@@ -0,0 +1,14 @@
+import { BaseRepository } from 'infra/db/baseRepository';
+import { ShortUrl } from '../shortUrl';
+
+export class DeleteShortUrl {
+	private readonly shortUrlRepository: BaseRepository<ShortUrl>;
+
+	constructor(shortUrlRepository: BaseRepository<ShortUrl>) {
+		this.shortUrlRepository = shortUrlRepository;
+	}
+
+	async execute(uuid: string): Promise<void> {
+		await this.shortUrlRepository.delete(uuid);
+	}
+}
