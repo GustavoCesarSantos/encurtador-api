fb70932f7756743e24cd00cce1b2be55d7b252ba
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-07 12:57:16 -0300

    refactor: Retira métodos e testes antigas da entidade shortUrl

--- __tests__/Unit/shortUrl.spec.ts
+++ __tests__/Unit/shortUrl.spec.ts
@@ -16,27 +16,4 @@ describe('Short url', () => {
 		const result = shortUrl.returnShortUrl(code);
 		expect(result).toBe(`${process.env.DOMAIN_URL}/${code}`);
 	});
-
-	test('Should return null if non-existent code is passed', () => {
-		const code = 'error';
-		const result = shortUrl.findRegistry(code);
-		expect(result).toBeNull();
-	});
-
-	test('Should return short url registry', () => {
-		const url = 'teste';
-		const code = '12345';
-		const result = shortUrl.findRegistry(code);
-		expect(result?.url).toBe(url);
-	});
-
-	test('Should add new hit', () => {
-		expect(() => shortUrl.addHit()).not.toThrow();
-	});
-
-	test('Should update url hits in infos', () => {
-		const id = '1';
-		const data = { hits: 2 };
-		expect(() => shortUrl.updateHits(id, data)).not.toThrow();
-	});
 });
