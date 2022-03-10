db8127e5433d4a75c1c677302583dc83e58be685
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-09 23:11:16 -0300

    refactor(shortUrl): Refatora entidade url encurtada

--- src/modules/shortUrls/shortUrl.ts
+++ src/modules/shortUrls/shortUrl.ts
@@ -3,13 +3,6 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-type ShortUrlSchema = {
-	id: string;
-	url: string;
-	code: string;
-	hits: number;
-};
-
 type ShortUrlConstructor = {
 	uuid?: string;
 	url: string;
@@ -20,35 +13,25 @@ type ShortUrlConstructor = {
 
 export class ShortUrl {
 	private readonly uuid: string;
-	private readonly url: string;
+	private readonly rootUrl: string;
 	private readonly code: string;
 	private readonly hits: number;
 	private readonly ownerid: number;
 	private createdat!: Date;
 
-	private shortUrls: ShortUrlSchema[] = [
-		{
-			id: '1',
-			url: 'teste',
-			code: '12345',
-			hits: 0,
-		},
-	];
-
 	constructor(props: ShortUrlConstructor) {
 		this.uuid = props.uuid ?? randomUUID();
-		this.url = props.url;
+		this.rootUrl = props.url;
 		this.code = props.code;
 		this.hits = props.hits ?? 0;
 		this.ownerid = props.ownerid ?? 0;
-		this.uuid = undefined ?? randomUUID();
 	}
 
 	setCreatedDate() {
 		this.createdat = new Date();
 	}
 
-	public returnShortUrl(code: string): string {
-		return `${process.env.DOMAIN_URL}/${code}`;
-	}
+	// public returnShortUrl(code: string): string {
+	// 	return `${process.env.DOMAIN_URL}/${code}`;
+	// }
 }
