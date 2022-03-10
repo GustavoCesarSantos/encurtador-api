09c0dde5a3cb79e7979ac38c74eec67d17f5aea8
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-10 18:20:55 -0300

    feat: Cria util para validar campos vazios ou indefinidos

---
+++ src/utils/guard.ts
@@ -0,0 +1,35 @@
+type propsList = {
+	propName: string;
+	value: string;
+};
+
+type Result = {
+	isSuccess: boolean;
+	isError: string[];
+};
+
+export class Guard {
+	static againstEmptyOrUndefined(propsList: propsList[]): Result {
+		const props: string[] = propsList.reduce(
+			(acc: string[], cur: propsList) => {
+				switch (cur.value) {
+					case undefined:
+						acc.push(cur.propName);
+						break;
+					case null:
+						acc.push(cur.propName);
+						break;
+					case '':
+						acc.push(cur.propName);
+						break;
+					default:
+						break;
+				}
+				return acc;
+			},
+			[],
+		);
+		if (props.length > 0) return { isSuccess: false, isError: props };
+		return { isSuccess: true, isError: [] };
+	}
+}
