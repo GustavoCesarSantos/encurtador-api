40c657a3bcb4e821dba542dd572e4466cef494f3
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-09 23:00:44 -0300

    refactor(shortUrl): Refatora teste unitario da entidade url encurtada

--- __tests__/Unit/shortUrl.spec.ts
+++ __tests__/Unit/shortUrl.spec.ts
@@ -1,19 +1,15 @@
 import { ShortUrl } from '@/shortUrls/shortUrl';
 
-let shortUrl: ShortUrl;
-
-const makeSut = (): ShortUrl => {
-	return new ShortUrl({ url: 'teste', code: 'teste' });
-};
-
 describe('Short url', () => {
-	beforeEach(() => {
-		shortUrl = makeSut();
+	test('Should create a entity short url', () => {
+		expect(
+			() => new ShortUrl({ url: 'teste', code: 'teste' }),
+		).not.toThrow();
 	});
 
-	test('Should return a valid shortened url', () => {
-		const code = '12345';
-		const result = shortUrl.returnShortUrl(code);
-		expect(result).toBe(`${process.env.DOMAIN_URL}/${code}`);
-	});
+	// test('Should return a valid shortened url', () => {
+	// 	const code = '12345';
+	// 	const result = shortUrl.returnShortUrl(code);
+	// 	expect(result).toBe(`${process.env.DOMAIN_URL}/${code}`);
+	// });
 });
