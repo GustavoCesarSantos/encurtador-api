26f0fc8cb281e8e0c5a1e032f158ad5d71fdb569
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-10 18:19:19 -0300

    refactor: Refatora entidade url encurtada

--- src/modules/shortUrls/shortUrl.ts
+++ src/modules/shortUrls/shortUrl.ts
@@ -1,5 +1,7 @@
 import { randomUUID } from 'crypto';
 import * as dotenv from 'dotenv';
+import { MissingParams } from '../../helpers/errors/missingParams';
+import { Guard } from '../../utils/guard';
 
 dotenv.config();
 
@@ -19,12 +21,21 @@ export class ShortUrl {
 	private readonly ownerid: number;
 	private createdat!: Date;
 
-	constructor(props: ShortUrlConstructor) {
+	private constructor(props: ShortUrlConstructor) {
 		this.uuid = props.uuid ?? randomUUID();
 		this.rootUrl = props.url;
 		this.code = props.code;
 		this.hits = props.hits ?? 0;
 		this.ownerid = props.ownerid ?? 0;
+	}
+
+	static create(props: ShortUrlConstructor) {
+		const result = Guard.againstEmptyOrUndefined([
+			{ propName: 'root url', value: props.url },
+			{ propName: 'code', value: props.code },
+		]);
+		if (!result.isSuccess) return new MissingParams(`${result.isError}`);
+		return new ShortUrl(props);
 	}
 
 	setCreatedDate() {
