9af61bbef27f46d5743c5199af35b10bad2d65b8
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-10 18:18:30 -0300

    refactor: Refatora teste unitario da entidade url encurtada

--- __tests__/Unit/shortUrl.spec.ts
+++ __tests__/Unit/shortUrl.spec.ts
@@ -1,10 +1,23 @@
 import { ShortUrl } from '@/shortUrls/shortUrl';
+import { MissingParams } from '../../src/helpers/errors/missingParams';
 
 describe('Short url', () => {
-	test('Should create a entity short url', () => {
-		expect(
-			() => new ShortUrl({ url: 'teste', code: 'teste' }),
-		).not.toThrow();
+	test('Should return an error when the url is not passed', () => {
+		const props = { url: '', code: '12345' };
+		const result = ShortUrl.create(props);
+		expect(result).toEqual(new MissingParams('root url'));
+	});
+
+	test('Should return an error when the code is not passed', () => {
+		const props = { url: 'teste', code: '' };
+		const result = ShortUrl.create(props);
+		expect(result).toEqual(new MissingParams('code'));
+	});
+
+	test('Should return a valid entity when all required params is passed', () => {
+		const props = { url: 'teste', code: '12345' };
+		const result = ShortUrl.create(props);
+		expect(result instanceof ShortUrl).toBe(true);
 	});
 
 	// test('Should return a valid shortened url', () => {
