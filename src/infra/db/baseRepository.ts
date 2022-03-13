da846659627c3fd9f6f808b42e8563f0678dda71
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-13 13:58:52 -0300

    refactor: Refatora método update da classe BaseRepository

--- src/infra/db/baseRepository.ts
+++ src/infra/db/baseRepository.ts
@@ -2,6 +2,6 @@ export interface BaseRepository<T> {
 	save(entity: T): Promise<void>;
 	findMany(): Promise<T[]>;
 	findOne(identifier: string): Promise<T | null>;
-	update(uuid: string, data: object): Promise<void>;
+	update(identifier: string, data: any): Promise<void>;
 	delete(uuid: string): Promise<void>;
 }
