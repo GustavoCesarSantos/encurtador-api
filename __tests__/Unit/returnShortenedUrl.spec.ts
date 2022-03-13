f05037a31ea160fbdf6b5b5106d0bea0c5683551
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-13 14:50:51 -0300

    feat: Cria teste unitario para o caso de uso retorna url encurtada

---
+++ __tests__/Unit/returnShortenedUrl.spec.ts
@@ -0,0 +1,28 @@
+import {
+	IReturnShortenedUrl,
+	ReturnShortenedUrl,
+} from '@/shortUrls/useCases/returnShortenedUrl';
+
+let returnShortenedUrl: IReturnShortenedUrl;
+
+const makeSut = () => {
+	return new ReturnShortenedUrl();
+};
+
+describe('Return shortened url', () => {
+	beforeEach(() => {
+		returnShortenedUrl = makeSut();
+	});
+
+	test('Should return a valid domain in url', () => {
+		const code = '12345';
+		const result = returnShortenedUrl.execute(code);
+		expect(result.split('/')[0]).toEqual(`${process.env.DOMAIN_URL}`);
+	});
+
+	test('Should return a valid code in url', () => {
+		const code = '12345';
+		const result = returnShortenedUrl.execute(code);
+		expect(result.split('/')[1]).toEqual(code);
+	});
+});
