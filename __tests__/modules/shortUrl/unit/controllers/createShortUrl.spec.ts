6aba128a05b60aae2cd0f134672b29a1112d5a61
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-20 01:15:40 -0300

    refactor: Refatora teste unitario

--- __tests__/modules/shortUrl/unit/controllers/createShortUrl.spec.ts
+++ __tests__/modules/shortUrl/unit/controllers/createShortUrl.spec.ts
@@ -1,10 +1,26 @@
 import { MissingParams } from '@helpers/errors/missingParams';
 import { CreateShortUrl } from '@modules/shortUrls/controllers/createShortUrl';
+import { IGenerateCode } from '@modules/shortUrls/useCases/generateCode';
+import { IReturnShortenedUrl } from '@modules/shortUrls/useCases/returnShortenedUrl';
 
 let saveShortUrl: any;
 
+class GenerateCodeDummie implements IGenerateCode {
+	execute(): string {
+		return '';
+	}
+}
+
+class ReturnShortenedUrlDummie implements IReturnShortenedUrl {
+	execute(code: string): string {
+		return '';
+	}
+}
+
 const makeSut = () => {
-	return new CreateShortUrl();
+	const generateCode = new GenerateCodeDummie();
+	const returnShortenedUrl = new ReturnShortenedUrlDummie();
+	return new CreateShortUrl({ generateCode, returnShortenedUrl });
 };
 
 describe('Create short url', () => {
@@ -13,40 +29,31 @@ describe('Create short url', () => {
 	});
 	test('Should return 400 when url is not provided', async () => {
 		const response = await saveShortUrl.handle({
-			body: { url: '', code: '12345' },
+			body: { url: '' },
 		});
 		expect(response.status).toBe(400);
 	});
 
 	test('Should return an missing params error when url is not provided', async () => {
 		const response = await saveShortUrl.handle({
-			body: { url: '', code: '12345' },
+			body: { url: '' },
 		});
 		expect(response.body).toStrictEqual({
 			message: new MissingParams('Url'),
 		});
 	});
 
-	test('Should return 400 when code is not provided', async () => {
+	test('Should return 201 when shortened url is created', async () => {
 		const response = await saveShortUrl.handle({
-			body: { url: 'teste', code: '' },
-		});
-		expect(response.status).toBe(400);
-	});
-
-	test('Should return an missing params error when code is not provided', async () => {
-		const response = await saveShortUrl.handle({
-			body: { url: 'teste', code: '' },
-		});
-		expect(response.body).toStrictEqual({
-			message: new MissingParams('Code'),
-		});
-	});
-
-	test('Should return 201 when code is created', async () => {
-		const response = await saveShortUrl.handle({
-			body: { url: 'teste', code: '12345' },
+			body: { url: 'teste' },
 		});
 		expect(response.status).toBe(201);
+	});
+
+	test('Should return a shortened url', async () => {
+		const response = await saveShortUrl.handle({
+			body: { url: 'teste' },
+		});
+		expect(response.body.url).not.toBeUndefined();
 	});
 });
