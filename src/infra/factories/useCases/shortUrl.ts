44d3842da0310f90b12336700e10ad2b72cd1e10
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-20 21:08:29 -0300

    feat: Cria fabrica de casos de uso

---
+++ src/infra/factories/useCases/shortUrl.ts
@@ -0,0 +1,36 @@
+import { ShortUrlRepository } from '@infra/db/prisma/shortUrlRepository';
+import { FindShortUrl } from '@modules/shortUrls/useCases/findShortUrl';
+import { GenerateCode } from '@modules/shortUrls/useCases/generateCode';
+import { IncrementHit } from '@modules/shortUrls/useCases/incrementHit';
+import { ReturnShortUrl } from '@modules/shortUrls/useCases/returnShortUrl';
+import { SaveShortUrl } from '@modules/shortUrls/useCases/saveShortUrl';
+import { UpdateShortUrl } from '@modules/shortUrls/useCases/updateShortUrl';
+
+export class ShortUrlUseCaseFactory {
+	private readonly shortUrlRepository: ShortUrlRepository =
+		new ShortUrlRepository();
+
+	public makeIncrementHit(): IncrementHit {
+		return new IncrementHit();
+	}
+
+	public makeFindShortUrl(): FindShortUrl {
+		return new FindShortUrl(this.shortUrlRepository);
+	}
+
+	public makeUpdateShortUrl(): UpdateShortUrl {
+		return new UpdateShortUrl(this.shortUrlRepository);
+	}
+
+	public makeGenerateCode(): GenerateCode {
+		return new GenerateCode();
+	}
+
+	public makeReturnShortUrl(): ReturnShortUrl {
+		return new ReturnShortUrl();
+	}
+
+	public makeSaveShortUrl(): SaveShortUrl {
+		return new SaveShortUrl(this.shortUrlRepository);
+	}
+}
