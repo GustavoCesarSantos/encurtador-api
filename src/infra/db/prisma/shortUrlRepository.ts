47f7f38f02be1645fb9f5d15e9e579fc9dcb87b7
Gustavo Cesar <gustavocs789@gmail.com>
2022-03-27 01:16:00 -0300

    refactor: Adiciona implementação ao método save do repositorio do prisma

--- src/infra/db/prisma/shortUrlRepository.ts
+++ src/infra/db/prisma/shortUrlRepository.ts
@@ -1,20 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+
 import { ShortUrl } from '@modules/shortUrls/shortUrl';
 import { BaseRepository } from '../baseRepository';
+import { ShortUrlMapper } from '@utils/mappers/shortUrlMapper';
 
-export class ShortUrlRepository implements BaseRepository<ShortUrl> {
-	async save(entity: ShortUrl): Promise<void> {
+export interface ISShortUrlRepository extends BaseRepository<ShortUrl> {
+	getShortUrlByCode(code: string): Promise<ShortUrl | null>;
+}
+
+export class ShortUrlRepository implements ISShortUrlRepository {
+	private readonly prisma;
+	private readonly mapper;
+
+	constructor() {
+		this.prisma = new PrismaClient();
+		this.mapper = new ShortUrlMapper();
+	}
+
+	public async save(entity: ShortUrl): Promise<void> {
+		const data = this.mapper.toPersistence(entity);
+		await this.prisma.shortUrls.create({ data });
+	}
+
+	getShortUrlByCode(code: string): Promise<ShortUrl | null> {
 		throw new Error('Method not implemented.');
 	}
-	async findMany(): Promise<ShortUrl[]> {
+
+	findMany(): Promise<ShortUrl[]> {
 		throw new Error('Method not implemented.');
 	}
-	async findOne(identifier: string): Promise<ShortUrl | null> {
+
+	findOne(identifier: string): Promise<ShortUrl | null> {
 		throw new Error('Method not implemented.');
 	}
-	async update(): Promise<void> {
+
+	update(identifier: string, data: object): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
-	async delete(): Promise<void> {
+
+	delete(uuid: string): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
 }
