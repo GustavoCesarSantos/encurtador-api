4253ea9712cf7e4bae9b05d6ca6f1f389ec076fa
GustavoCesarSantos <gustavocs789@gmail.com>
2022-04-10 01:30:48 -0300

    feat: Adiciona implementação da interface listener e método update na classe pino logger

--- src/infra/listeners/loggers/pinoLogger.ts
+++ src/infra/listeners/loggers/pinoLogger.ts
@@ -1,7 +1,10 @@
 import pino, { Logger } from 'pino';
+
+import { IListener } from '../listener';
+import { EventNames } from '@helpers/eventNames';
 import { ILogger } from './logger';
 
-export class PinoLogger implements ILogger {
+export class PinoLogger implements ILogger, IListener {
 	private transport: any = {};
 	private readonly logger!: Logger<any>;
 
@@ -20,6 +23,19 @@ export class PinoLogger implements ILogger {
 
 	public static create(): PinoLogger {
 		return new PinoLogger();
+	}
+
+	public update(eventName: string, payload: string | Error): void {
+		if (eventName === EventNames.info) {
+			if (typeof payload === 'string') return this.info(payload);
+			return this.error(
+				`Tipo invalido de payload: ${typeof payload} para evento: ${eventName}`,
+			);
+		}
+		if (eventName === EventNames.error) return this.error(payload);
+		this.error(
+			`Incapaz de gerar logs para este tipo de evento: ${eventName}`,
+		);
 	}
 
 	public child(binding: pino.Bindings): Logger<any> {
