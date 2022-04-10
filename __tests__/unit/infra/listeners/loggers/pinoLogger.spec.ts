4971f632654c52d6b2171013ca8b768f589390f6
GustavoCesarSantos <gustavocs789@gmail.com>
2022-04-10 18:17:04 -0300

    refactor: Refatora teste unitario do pino logger

--- __tests__/unit/infra/listeners/loggers/pinoLogger.spec.ts
+++ __tests__/unit/infra/listeners/loggers/pinoLogger.spec.ts
@@ -2,49 +2,33 @@ import { EventNames } from '@helpers/eventNames';
 import { PinoLogger } from '@infra/listeners/loggers/pinoLogger';
 
 describe('Pino Logger', () => {
-	test('Should call error log method when invalid type is passed in info event payload', () => {
-		const logger = PinoLogger.create();
-		const spy = jest.spyOn(logger, 'error');
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		/**@ts-ignore */
-		logger.update(EventNames.info, 1);
-		expect(spy).toHaveBeenCalled();
-	});
-
-	test('Should call error log method when invalid type is passed in error event payload', () => {
-		const logger = PinoLogger.create();
-		const spy = jest.spyOn(logger, 'error');
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		/**@ts-ignore */
-		logger.update(EventNames.error, 1);
-		expect(spy).toHaveBeenCalled();
-	});
-
 	test('Should call error log method when invalid an event name is passed', () => {
 		const logger = PinoLogger.create();
 		const spy = jest.spyOn(logger, 'error');
-		logger.update('fail', 'fail');
+		logger.update({
+			eventName: 'fail',
+			message: { where: 'fail', what: 'fail' },
+		});
 		expect(spy).toHaveBeenCalled();
 	});
 
-	test('Should call info log method when event type is info and payload type is a string', () => {
+	test('Should call info log method when event type is info', () => {
 		const logger = PinoLogger.create();
 		const spy = jest.spyOn(logger, 'info');
-		logger.update(EventNames.info, 'success');
+		logger.update({
+			eventName: EventNames.info,
+			message: { where: 'success', what: 'success' },
+		});
 		expect(spy).toHaveBeenCalled();
 	});
 
-	test('Should call error log method when event type is error and payload type is a string', () => {
+	test('Should call error log method when event type is error', () => {
 		const logger = PinoLogger.create();
 		const spy = jest.spyOn(logger, 'error');
-		logger.update(EventNames.error, 'some error');
-		expect(spy).toHaveBeenCalled();
-	});
-
-	test('Should call error log method when event type is error and payload is a instance of error', () => {
-		const logger = PinoLogger.create();
-		const spy = jest.spyOn(logger, 'error');
-		logger.update(EventNames.error, new Error('some error'));
+		logger.update({
+			eventName: EventNames.error,
+			message: { where: 'success', what: 'success' },
+		});
 		expect(spy).toHaveBeenCalled();
 	});
 });
