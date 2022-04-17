c2fb0c6bd3728c977cc26abfd3b32099c5ce04ed
GustavoCesarSantos <gustavocs789@gmail.com>
2022-04-17 18:34:31 -0300

    feat: Adiciona logs a classe createShortUrl

--- src/modules/shortUrls/controllers/createShortUrl.ts
+++ src/modules/shortUrls/controllers/createShortUrl.ts
@@ -20,13 +20,13 @@ export class CreateShortUrl implements IController<Request> {
 	private readonly generateCode: IGenerateCode;
 	private readonly returnShortUrl: IReturnShortUrl;
 	private readonly saveShortUrl: ISaveShortUrl;
-	private readonly manager: IEventManager;
+	private readonly eventManager: IEventManager;
 
-	constructor(factory: IShortUrlUseCaseFactory, eventMangaer: IEventManager) {
+	constructor(factory: IShortUrlUseCaseFactory, eventManager: IEventManager) {
 		this.generateCode = factory.makeGenerateCode();
 		this.returnShortUrl = factory.makeReturnShortUrl();
 		this.saveShortUrl = factory.makeSaveShortUrl();
-		this.manager = eventMangaer;
+		this.eventManager = eventManager;
 	}
 
 	public async handle(request: Request): Promise<Response> {
@@ -35,16 +35,79 @@ export class CreateShortUrl implements IController<Request> {
 			{ propName: 'Url', value: url },
 		]);
 		if (!result.isSuccess) {
+			this.eventManager.notify({
+				eventName: EventNames.error,
+				message: {
+					where: 'CreateShortUrl',
+					what: new MissingParams(`${result.isError}`).message,
+				},
+			});
 			return HttpResponse.badRequest(
 				new MissingParams(`${result.isError}`),
 			);
 		}
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'CreateShortUrl',
+				what: `Iniciando criação do código para a url: ${url}`,
+			},
+		});
 		const code = this.generateCode.execute();
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'CreateShortUrl',
+				what: `Código: ${code} para a url: ${url} criado`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'CreateShortUrl',
+				what: `Iniciando criação da url encurtada, utilizando o código: ${code}, url original: ${url}`,
+			},
+		});
 		const shortUrl = this.returnShortUrl.execute(code);
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'CreateShortUrl',
+				what: `Url encurtada: ${shortUrl} criada, utilizando o código: ${code}, url original: ${url}`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'CreateShortUrl',
+				what: `Iniciando persistencia dos dados da url encurtada: ${shortUrl}, código: ${code}, url original: ${url}`,
+			},
+		});
 		const error = await this.saveShortUrl.execute(url, code);
 		if (error) {
+			this.eventManager.notify({
+				eventName: EventNames.error,
+				message: {
+					where: 'CreateShortUrl',
+					what: `Persistencia dos dados da url encurtada: ${shortUrl} falhou, error: ${error.message}`,
+				},
+			});
 			return HttpResponse.badRequest(error);
 		}
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'CreateShortUrl',
+				what: `Persistencia dos dados da url encurtada: ${shortUrl} concluida`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'CreateShortUrl',
+				what: `Retornando a resposta: ${{ url: shortUrl }}`,
+			},
+		});
 		return HttpResponse.okWithBody({ url: shortUrl });
 	}
 }
