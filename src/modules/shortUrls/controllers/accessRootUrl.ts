ab8dffa06ea370fdb8997acee79b827ed8a09969
GustavoCesarSantos <gustavocs789@gmail.com>
2022-04-24 13:37:51 -0300

    feat: Adiciona parametro event manager ao construtor da classe access root url

--- src/modules/shortUrls/controllers/accessRootUrl.ts
+++ src/modules/shortUrls/controllers/accessRootUrl.ts
@@ -7,6 +7,7 @@ import { IFindShortUrl } from '../useCases/findShortUrl';
 import { IIncrementHit } from '../useCases/incrementHit';
 import { IUpdateShortUrl } from '../useCases/updateShortUrl';
 import { IShortUrlUseCaseFactory } from '@infra/factories/useCases/IShortUrlUseCaseFactory';
+import { IEventManager } from '@infra/listeners/eventManager';
 
 type Request = {
 	params: {
@@ -18,11 +19,13 @@ export class AccessRootUrl implements IController<Request> {
 	private readonly findShortUrl: IFindShortUrl;
 	private readonly incrementHit: IIncrementHit;
 	private readonly updateShortUrl: IUpdateShortUrl;
+	private readonly eventManager: IEventManager;
 
-	constructor(factory: IShortUrlUseCaseFactory) {
+	constructor(factory: IShortUrlUseCaseFactory, eventManager: IEventManager) {
 		this.findShortUrl = factory.makeFindShortUrl();
 		this.incrementHit = factory.makeIncrementHit();
 		this.updateShortUrl = factory.makeUpdateShortUrl();
+		this.eventManager = eventManager;
 	}
 
 	public async handle(request: Request): Promise<Response> {
