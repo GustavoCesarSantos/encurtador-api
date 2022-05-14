c0024c08851008b0a28a155949d2ed9b2a62d99a
GustavoCesarSantos <gustavocs789@gmail.com>
2022-05-14 17:00:43 -0300

    refactor: :recycle: refatora repositorio que utiliza prisma

--- src/infra/db/prisma/shortUrlRepositoryWithPrisma.ts
+++ src/infra/db/prisma/shortUrlRepositoryWithPrisma.ts
@@ -1,29 +1,26 @@
-import { PrismaClient } from '@prisma/client';
-
 import { ShortUrl } from '@modules/shortUrls/shortUrl';
 import { ShortUrlMapper } from '@utils/mappers/shortUrlMapper';
 import { IShortUrlRepository } from '../shortUrlRepository';
+import { prisma } from './prismaHelper';
 
 export class ShortUrlRepositoryWithPrisma implements IShortUrlRepository {
 	private readonly prisma;
 	private readonly mapper;
 
 	constructor() {
-		this.prisma = new PrismaClient();
+		this.prisma = prisma;
 		this.mapper = new ShortUrlMapper();
 	}
 
 	public async save(entity: ShortUrl): Promise<void> {
 		const data = this.mapper.toPersistence(entity);
 		await this.prisma.shortUrls.create({ data });
-		await this.prisma.$disconnect();
 	}
 
 	public async getShortUrlByCode(code: string): Promise<ShortUrl | null> {
 		const shortUrlDB = await this.prisma.shortUrls.findUnique({
 			where: { code },
 		});
-		await this.prisma.$disconnect();
 		if (!shortUrlDB) return null;
 		return this.mapper.toDomain(shortUrlDB);
 	}
@@ -34,7 +31,6 @@ export class ShortUrlRepositoryWithPrisma implements IShortUrlRepository {
 		const shortUrlsDB = await this.prisma.shortUrls.findMany({
 			where: { ownerid: ownerId },
 		});
-		await this.prisma.$disconnect();
 		const shortUrls: ShortUrl[] = [];
 		const result = shortUrlsDB.every(shortUrlDB => {
 			const shortUrl = this.mapper.toDomain(shortUrlDB);
@@ -51,7 +47,6 @@ export class ShortUrlRepositoryWithPrisma implements IShortUrlRepository {
 			where: { uuid },
 			data: data,
 		});
-		await this.prisma.$disconnect();
 	}
 
 	public async delete(uuid: string): Promise<void> {
