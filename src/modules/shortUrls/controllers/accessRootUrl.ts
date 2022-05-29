cc0bef60927b58179ec04668f752c3287d292fc6
GustavoCesarSantos <gustavocs789@gmail.com>
2022-05-29 15:34:59 -0300

    refactor: adiciona bloco try catch a controller accessRootUrl

--- src/modules/shortUrls/controllers/accessRootUrl.ts
+++ src/modules/shortUrls/controllers/accessRootUrl.ts
@@ -30,88 +30,92 @@ export class AccessRootUrl implements IController<Request> {
 	}
 
 	public async handle(request: Request): Promise<Response> {
-		const { code } = request.params;
-		const result = Guard.againstEmptyOrUndefined([
-			{ propName: 'Code', value: code },
-		]);
-		if (!result.isSuccess) {
+		try {
+			const { code } = request.params;
+			const result = Guard.againstEmptyOrUndefined([
+				{ propName: 'Code', value: code },
+			]);
+			if (!result.isSuccess) {
+				this.eventManager.notify({
+					eventName: EventNames.error,
+					message: {
+						where: 'AccessRootUrl',
+						what: new MissingParams(`${result.isError}`).message,
+					},
+				});
+				return HttpResponse.badRequest(
+					new MissingParams(`${result.isError}`),
+				);
+			}
 			this.eventManager.notify({
-				eventName: EventNames.error,
+				eventName: EventNames.info,
 				message: {
 					where: 'AccessRootUrl',
-					what: new MissingParams(`${result.isError}`).message,
+					what: `Iniciando busca pela url encurtada, utilizando o código: ${code}`,
 				},
 			});
-			return HttpResponse.badRequest(
-				new MissingParams(`${result.isError}`),
-			);
-		}
-		this.eventManager.notify({
-			eventName: EventNames.info,
-			message: {
-				where: 'AccessRootUrl',
-				what: `Iniciando busca pela url encurtada, utilizando o código: ${code}`,
-			},
-		});
-		const shortUrl = await this.findShortUrl.execute(code);
-		if (!shortUrl) {
+			const shortUrl = await this.findShortUrl.execute(code);
+			if (!shortUrl) {
+				this.eventManager.notify({
+					eventName: EventNames.error,
+					message: {
+						where: 'AccessRootUrl',
+						what: `Url encurtada não encontrada utilizando o código: ${code}`,
+					},
+				});
+				return HttpResponse.notFound();
+			}
 			this.eventManager.notify({
-				eventName: EventNames.error,
+				eventName: EventNames.info,
 				message: {
 					where: 'AccessRootUrl',
-					what: `Url encurtada não encontrada utilizando o código: ${code}`,
+					what: `Url encurtada: ${shortUrl} encontrada, utilizando o código: ${code}`,
 				},
 			});
-			return HttpResponse.notFound();
-		}
-		this.eventManager.notify({
-			eventName: EventNames.info,
-			message: {
-				where: 'AccessRootUrl',
-				what: `Url encurtada: ${shortUrl} encontrada, utilizando o código: ${code}`,
-			},
-		});
-		this.eventManager.notify({
-			eventName: EventNames.info,
-			message: {
-				where: 'AccessRootUrl',
-				what: `Iniciando incremento do número de acessos a url, número atual: ${shortUrl.getHits()}`,
-			},
-		});
-		const hits = await this.incrementHit.execute(shortUrl.getHits());
-		this.eventManager.notify({
-			eventName: EventNames.info,
-			message: {
-				where: 'AccessRootUrl',
-				what: `Incremento no número de acessos a url feito com sucesso, número atual: ${hits}`,
-			},
-		});
-		this.eventManager.notify({
-			eventName: EventNames.info,
-			message: {
-				where: 'AccessRootUrl',
-				what: `Iniciando atualização do schema da url encurtada: ${shortUrl.getUUID()}, 
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Iniciando incremento do número de acessos a url, número atual: ${shortUrl.getHits()}`,
+				},
+			});
+			const hits = await this.incrementHit.execute(shortUrl.getHits());
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Incremento no número de acessos a url feito com sucesso, número atual: ${hits}`,
+				},
+			});
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Iniciando atualização do schema da url encurtada: ${shortUrl.getUUID()}, 
 				informações para atualizar: ${JSON.stringify({ hits })}`,
-			},
-		});
-		await this.updateShortUrl.execute(shortUrl.getUUID(), { hits });
-		this.eventManager.notify({
-			eventName: EventNames.info,
-			message: {
-				where: 'AccessRootUrl',
-				what: `Schema da url encurtada: ${shortUrl.getUUID()} atualizado com sucesso, 
+				},
+			});
+			await this.updateShortUrl.execute(shortUrl.getUUID(), { hits });
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Schema da url encurtada: ${shortUrl.getUUID()} atualizado com sucesso, 
 				informações atualizadas: ${JSON.stringify({ hits })}`,
-			},
-		});
-		this.eventManager.notify({
-			eventName: EventNames.info,
-			message: {
-				where: 'AccessRootUrl',
-				what: `Retornando a resposta: ${JSON.stringify({
-					rootUrl: shortUrl.getRootUrl(),
-				})}`,
-			},
-		});
-		return HttpResponse.redirect({ rootUrl: shortUrl.getRootUrl() });
+				},
+			});
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Retornando a resposta: ${JSON.stringify({
+						rootUrl: shortUrl.getRootUrl(),
+					})}`,
+				},
+			});
+			return HttpResponse.redirect({ rootUrl: shortUrl.getRootUrl() });
+		} catch (error) {
+			return HttpResponse.serverError();
+		}
 	}
 }
