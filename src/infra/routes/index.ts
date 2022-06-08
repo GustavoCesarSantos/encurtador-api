e808eb6a3dcea4cab4dc675781c21dafb2246ee3
GustavoCesarSantos <gustavocs789@gmail.com>
2022-06-08 14:31:12 -0300

    feat: adiciona as rotas de health check ao router do express

--- src/infra/routes/index.ts
+++ src/infra/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
 import { shortUrl } from './shortUrl';
+import { healthCheck } from './healthCheck';
 
 export const routes = (router: Router) => {
 	shortUrl(router);
+	healthCheck(router);
 };
