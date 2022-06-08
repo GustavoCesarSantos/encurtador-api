55fbf7ad3500cbd27de515f1ce3bde11575c7c79
GustavoCesarSantos <gustavocs789@gmail.com>
2022-06-08 14:32:44 -0300

    refactor(short url): altera rota

--- __tests__/e2e/accessRootUrl.spec.ts
+++ __tests__/e2e/accessRootUrl.spec.ts
@@ -7,33 +7,41 @@ app.setupRoutes();
 
 describe('Access root url', () => {
 	test('Should return status code 404 when schema is not found', async () => {
-		const { status } = await request(app.getApp()).get('/v1/fail');
+		const { status } = await request(app.getApp()).get(
+			'/v1/shortenedUrls/fail',
+		);
 		expect(status).toBe(404);
 	});
 
 	test('Should return not found message when code is not found', async () => {
-		const { text } = await request(app.getApp()).get('/v1/fail');
+		const { text } = await request(app.getApp()).get(
+			'/v1/shortenedUrls/fail',
+		);
 		const body = JSON.parse(text);
 		expect(body.error.message).toBe('Not found');
 	});
 
 	test('Should return status code 302', async () => {
 		const { text } = await request(app.getApp())
-			.post('/v1/')
+			.post('/v1/shortenedUrls')
 			.send({ url: 'success' });
 		const body = JSON.parse(text);
 		const code: string = body.url.split('v1/')[1];
-		const { status } = await request(app.getApp()).get(`/v1/${code}`);
+		const { status } = await request(app.getApp()).get(
+			`/v1/shortenedUrls/${code}`,
+		);
 		expect(status).toBe(302);
 	});
 
 	test('Should redirect to root url', async () => {
 		const { text } = await request(app.getApp())
-			.post('/v1/')
+			.post('/v1/shortenedUrls')
 			.send({ url: 'success' });
 		const body = JSON.parse(text);
 		const code: string = body.url.split('v1/')[1];
-		const { redirect } = await request(app.getApp()).get(`/v1/${code}`);
+		const { redirect } = await request(app.getApp()).get(
+			`/v1/shortenedUrls/${code}`,
+		);
 		expect(redirect).toBe(true);
 	});
 });
--- __tests__/e2e/createShortUrl.spec.ts
+++ __tests__/e2e/createShortUrl.spec.ts
@@ -7,26 +7,30 @@ app.setupRoutes();
 
 describe('Create short url', () => {
 	test('Should return status code 400 when url is not passed in request body', async () => {
-		const { status } = await request(app.getApp()).post('/v1/').send({});
+		const { status } = await request(app.getApp())
+			.post('/v1/shortenedUrls')
+			.send({});
 		expect(status).toBe(400);
 	});
 
 	test('Should return error message with missing param when url is not passed in request body', async () => {
-		const { text } = await request(app.getApp()).post('/v1/').send({});
+		const { text } = await request(app.getApp())
+			.post('/v1/shortenedUrls')
+			.send({});
 		const body = JSON.parse(text);
 		expect(body.error.message).toBe('Missing params: Url');
 	});
 
 	test('Should return status code 201 when valid params are passed', async () => {
 		const { status } = await request(app.getApp())
-			.post('/v1/')
+			.post('/v1/shortenedUrls')
 			.send({ url: 'success' });
 		expect(status).toBe(201);
 	});
 
 	test('Should return a short url when valid params are passed', async () => {
 		const { text } = await request(app.getApp())
-			.post('/v1/')
+			.post('/v1/shortenedUrls')
 			.send({ url: 'success' });
 		const body = JSON.parse(text);
 		expect(body).toHaveProperty('url');
