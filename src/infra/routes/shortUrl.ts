63221d962afdf9ae533cabf46c496698995641cb
GustavoCesarSantos <gustavocs789@gmail.com>
2022-09-25 22:12:31 -0300

    refactor: :recycle: refatora nome do adaptador de rota

--- src/infra/routes/shortUrl.ts
+++ src/infra/routes/shortUrl.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { expressRouteAdapter } from '@infra/adapters/expressRouteAdapter';
+import { adaptRoute } from '@infra/adapters/expressRouteAdapter';
 import { ShortUrlControllerFactory } from '@infra/factories/controllers/shortUrl';
 
 const shortUrlControllerFactory = new ShortUrlControllerFactory();
@@ -8,10 +8,10 @@ const shortUrlControllerFactory = new ShortUrlControllerFactory();
 export const shortUrl = (router: Router): void => {
 	router.get(
 		'/shortenedUrls/:code',
-		expressRouteAdapter(shortUrlControllerFactory.makeAccessRootUrl()),
+		adaptRoute(shortUrlControllerFactory.makeAccessRootUrl()),
 	);
 	router.post(
 		'/shortenedUrls',
-		expressRouteAdapter(shortUrlControllerFactory.makeCreateShortUrl()),
+		adaptRoute(shortUrlControllerFactory.makeCreateShortUrl()),
 	);
 };
