f0d04ccbea5a09d0df0e3750b9236ffdd0966d95
GustavoCesarSantos <gustavocs789@gmail.com>
2022-10-30 15:32:45 -0300

    feat: adiciona rate limit

--- src/infra/routes/shortUrl.ts
+++ src/infra/routes/shortUrl.ts
@@ -2,16 +2,19 @@ import { Router } from 'express';
 
 import { adaptRoute } from '@infra/adapters/expressRouteAdapter';
 import { ShortUrlControllerFactory } from '@infra/factories/controllers/shortUrl';
+import { rateLimit } from '@infra/factories/middlewares/rateLimit';
 
 const shortUrlControllerFactory = new ShortUrlControllerFactory();
 
 export const shortUrl = (router: Router): void => {
 	router.get(
 		'/shortenedUrls/:code',
+		rateLimit,
 		adaptRoute(shortUrlControllerFactory.makeAccessRootUrl()),
 	);
 	router.post(
 		'/shortenedUrls',
+		rateLimit,
 		adaptRoute(shortUrlControllerFactory.makeCreateShortUrl()),
 	);
 };
