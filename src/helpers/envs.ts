5682f1106e5fbc824c74cd3d01d7fca3f1b8e3da
GustavoCesarSantos <gustavocs789@gmail.com>
2023-01-08 17:14:17 -0300

    ci: :green_heart: adiciona variavel de ambiente para url do redis

--- src/helpers/envs.ts
+++ src/helpers/envs.ts
@@ -5,6 +5,11 @@ export const variables = {
 	domainUrl: String(process.env.DOMAIN_URL) ?? 'http://localhost:3000/v1',
 	port: String(process.env.PORT) ?? '3001',
 	nodeEnv: String(process.env.NODE_ENV) ?? 'development',
+	redisUrl: String(
+		`redis://:${process.env.REDIS_PASSWORD ?? 'devTestePassword'}@${
+			process.env.REDIS_HOST ?? 'localhost'
+		}:${process.env.REDIS_PORT ?? '6379'}`,
+	),
 	rateLimitFixedWindowInMinutes:
 		Number(process.env.RATE_LIMIT_FIXED_WINDOW_IN_MINUTES) ?? 90,
 	rateLimitToken: Number(process.env.RATE_LIMIT_TOKEN) ?? 1000,
