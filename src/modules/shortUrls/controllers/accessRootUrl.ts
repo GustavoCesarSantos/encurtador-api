2d80f974cf47ea34ef59dd2cccf83054f2fc1c97
GustavoCesarSantos <gustavocs789@gmail.com>
2023-01-09 10:22:23 -0300

    feat: adiciona fila de atualização

--- src/modules/shortUrls/controllers/accessRootUrl.ts
+++ src/modules/shortUrls/controllers/accessRootUrl.ts
@@ -3,10 +3,11 @@ import { Guard } from '@utils/guard';
 import { HttpResponse } from '@helpers/httpResponse';
 import { ICache } from '@infra/cache/ICache';
 import { IController } from '@shared/IController';
+import { IEventManager } from '@infra/listeners/eventManager';
 import { IFindShortUrl } from '../useCases/findShortUrl';
 import { IIncrementHit } from '../useCases/incrementHit';
+import { IQueue } from '@infra/queues/IQueue';
 import { IShortUrlUseCaseFactory } from '@infra/factories/useCases/IShortUrlUseCaseFactory';
-import { IEventManager } from '@infra/listeners/eventManager';
 import { MissingParams } from '@helpers/errors/missingParams';
 import { Response } from '@shared/response';
 
@@ -20,19 +21,22 @@ type ShortenedUrl = { url: string; hits: number };
 
 export class AccessRootUrl implements IController<Request> {
 	private readonly cache: ICache;
+	private readonly eventManager: IEventManager;
 	private readonly findShortUrl: IFindShortUrl;
 	private readonly incrementHit: IIncrementHit;
-	private readonly eventManager: IEventManager;
+	private readonly queue: IQueue;
 
 	constructor(
 		factory: IShortUrlUseCaseFactory,
 		eventManager: IEventManager,
 		cache: ICache,
+		queue: IQueue,
 	) {
 		this.cache = cache;
+		this.eventManager = eventManager;
 		this.findShortUrl = factory.makeFindShortUrl();
 		this.incrementHit = factory.makeIncrementHit();
-		this.eventManager = eventManager;
+		this.queue = queue;
 	}
 
 	public async handle(request: Request): Promise<Response> {
@@ -81,6 +85,26 @@ export class AccessRootUrl implements IController<Request> {
 					what: `Incremento no número de acessos a url feito com sucesso, número atual: ${hits}`,
 				},
 			});
+			const jobData = { code, hits };
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Enviando dados para a fila de atualização de hits da url encurtada. Data: ${JSON.stringify(
+						jobData,
+					)}`,
+				},
+			});
+			await this.queue.add('update', jobData);
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Dados enviados para a fila de atualização de hits da url encurtada. Data: ${JSON.stringify(
+						jobData,
+					)}`,
+				},
+			});
 			this.eventManager.notify({
 				eventName: EventNames.info,
 				message: {
@@ -88,7 +112,7 @@ export class AccessRootUrl implements IController<Request> {
 					what: `Iniciando atualização da camada de cache`,
 				},
 			});
-			await this.updateCache(code, shortenedUrl.url, shortenedUrl.hits);
+			await this.updateCache(code, shortenedUrl.url, hits);
 			this.eventManager.notify({
 				eventName: EventNames.info,
 				message: {
@@ -96,7 +120,6 @@ export class AccessRootUrl implements IController<Request> {
 					what: `Camada de cache atualizada`,
 				},
 			});
-			//Adicionar aqui a fila de atualização de hits
 			this.eventManager.notify({
 				eventName: EventNames.info,
 				message: {
