97a59a639f140cb17f4568cd7271cbed82c3e639
GustavoCesarSantos <gustavocs789@gmail.com>
2023-01-10 11:01:45 -0300

    fix: :bug: corrige uso de variaveis de ambiente

--- src/helpers/envs.ts
+++ src/helpers/envs.ts
@@ -3,16 +3,15 @@ dotenv.config();
 
 export const variables = {
 	domainUrl: process.env.DOMAIN_URL ?? 'http://localhost:3000/v1',
-	port: String(process.env.PORT) ?? '3001',
-	nodeEnv: String(process.env.NODE_ENV) ?? 'development',
-	redisUrl: String(
-		`redis://:${process.env.REDIS_PASSWORD ?? 'devTestePassword'}@${
-			process.env.REDIS_HOST ?? 'localhost'
-		}:${process.env.REDIS_PORT ?? '6379'}`,
+	port: process.env.PORT ?? '3001',
+	nodeEnv: process.env.NODE_ENV ?? 'development',
+	redisUrl: `redis://:${process.env.REDIS_PASSWORD ?? 'devTestePassword'}@${
+		process.env.REDIS_HOST ?? 'localhost'
+	}:${process.env.REDIS_PORT ?? '6379'}`,
+	rateLimitFixedWindowInMinutes: Number(
+		process.env.RATE_LIMIT_FIXED_WINDOW_IN_MINUTES ?? 90,
 	),
-	rateLimitFixedWindowInMinutes:
-		Number(process.env.RATE_LIMIT_FIXED_WINDOW_IN_MINUTES) ?? 90,
-	rateLimitToken: Number(process.env.RATE_LIMIT_TOKEN) ?? 1000,
+	rateLimitToken: Number(process.env.RATE_LIMIT_TOKEN ?? 1000),
 	shortenedUrlCreatedQueue: 'shortened-url-created-queue',
 	shortenedUrlHitsUpdatedQueue: 'shortened-url-hits-updated-queue',
 };
