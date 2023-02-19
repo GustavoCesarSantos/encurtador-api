228e47717ca37a61167344316fdd38ceff40e080
GustavoCesarSantos <gustavocs789@gmail.com>
2023-02-19 14:15:24 -0300

    refactor: adiciona logs a controller de acesso a url original

--- src/modules/shortUrls/controllers/accessRootUrl.ts
+++ src/modules/shortUrls/controllers/accessRootUrl.ts
@@ -37,11 +37,37 @@ export class AccessRootUrl implements IController<Request> {
 
 	public async handle(request: Request): Promise<Response> {
 		try {
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Iniciando requisição. Params: ${JSON.stringify(
+						request.params,
+					)}`,
+				},
+			});
 			const { code } = request.params;
-			const result = Guard.againstEmptyOrUndefined([
-				{ propName: 'Code', value: code },
-			]);
+			const validationSchema = [{ propName: 'Code', value: code }];
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Iniciando validação dos params: ${JSON.stringify(
+						validationSchema,
+					)} `,
+				},
+			});
+			const result = Guard.againstEmptyOrUndefined(validationSchema);
 			if (!result.isSuccess) {
+				this.eventManager.notify({
+					eventName: EventNames.error,
+					message: {
+						where: 'AccessRootUrl',
+						what: `Params invalido: ${JSON.stringify(
+							validationSchema,
+						)}`,
+					},
+				});
 				this.eventManager.notify({
 					eventName: EventNames.error,
 					message: {
@@ -53,8 +79,29 @@ export class AccessRootUrl implements IController<Request> {
 					new MissingParams(`${result.isError}`),
 				);
 			}
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Params validado com sucessso.`,
+				},
+			});
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'CreateShortUrl',
+					what: `Iniciando retorno da url original utilizando o código: ${code}.`,
+				},
+			});
 			const rootUrl = await this.getRootUrl(code);
 			if (!rootUrl) {
+				this.eventManager.notify({
+					eventName: EventNames.error,
+					message: {
+						where: 'AccessRootUrl',
+						what: `Url original não encontrada utilizando o código: ${code}.`,
+					},
+				});
 				this.eventManager.notify({
 					eventName: EventNames.error,
 					message: {
@@ -66,7 +113,21 @@ export class AccessRootUrl implements IController<Request> {
 				});
 				return HttpResponse.notFound();
 			}
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Iniciando envio para a fila de atualização de hits da url encurtada.`,
+				},
+			});
 			await this.sendToShortenedUrlHitsUpdatedQueue('1');
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl',
+					what: `Envio para a fila de atualização de hits da url encurtada feito com sucesso.`,
+				},
+			});
 			this.eventManager.notify({
 				eventName: EventNames.info,
 				message: {
@@ -94,16 +155,16 @@ export class AccessRootUrl implements IController<Request> {
 			eventName: EventNames.info,
 			message: {
 				where: 'AccessRootUrl.getRootUrlFromCache',
-				what: `Iniciando busca pela url na camada de cache, utilizando o código: ${code}`,
+				what: `Iniciando busca pela url original na camada de cache, utilizando o código: ${code}`,
 			},
 		});
-		const shortenedUrlCache = await this.cache.get(code);
-		if (!shortenedUrlCache) {
+		const rootUrlCache = await this.cache.get(code);
+		if (!rootUrlCache) {
 			this.eventManager.notify({
 				eventName: EventNames.warn,
 				message: {
 					where: 'AccessRootUrl.getRootUrlFromCache',
-					what: `Url não encontrada na camada de cache. key: ${code}`,
+					what: `Url original não encontrada na camada de cache utilizando o código: ${code}.`,
 				},
 			});
 			return null;
@@ -112,27 +173,27 @@ export class AccessRootUrl implements IController<Request> {
 			eventName: EventNames.info,
 			message: {
 				where: 'AccessRootUrl.getRootUrlFromCache',
-				what: `Url encontrada na camada de cache. key: ${code}, value: ${shortenedUrlCache}`,
+				what: `Url original encontrada na camada de cache. Code: ${code}, Url: ${rootUrlCache}`,
 			},
 		});
-		return shortenedUrlCache;
+		return rootUrlCache;
 	}
 
 	private async getRootUrlFromDB(code: string): Promise<string | null> {
 		this.eventManager.notify({
 			eventName: EventNames.info,
 			message: {
-				where: 'AccessRootUrl',
-				what: `Iniciando busca pela url encurtada no banco de dados, utilizando o código: ${code}`,
+				where: 'AccessRootUrl.getRootUrlFromDB',
+				what: `Iniciando busca pelo registro da url encurtada no banco de dados, utilizando o código: ${code}`,
 			},
 		});
 		const shortenedUrl = await this.findShortUrl.execute(code);
 		if (!shortenedUrl) {
 			this.eventManager.notify({
-				eventName: EventNames.error,
+				eventName: EventNames.warn,
 				message: {
-					where: 'AccessRootUrl',
-					what: `Url encurtada não encontrada no banco de dados utilizando o código: ${code}`,
+					where: 'AccessRootUrl.getRootUrlFromDB',
+					what: `Registro da url encurtada não encontrada no banco de dados utilizando o código: ${code}`,
 				},
 			});
 			return null;
@@ -140,8 +201,15 @@ export class AccessRootUrl implements IController<Request> {
 		this.eventManager.notify({
 			eventName: EventNames.info,
 			message: {
-				where: 'AccessRootUrl',
-				what: `Url encurtada encontrada no banco de dados, utilizando o código: ${code}`,
+				where: 'AccessRootUrl.getRootUrlFromDB',
+				what: `Registro da url encurtada encontrada no banco de dados, utilizando o código: ${code}`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.getRootUrlFromDB',
+				what: `Retornando url original do registro encontrado no banco de dados, utilizando o código: ${code}`,
 			},
 		});
 		return shortenedUrl.getRootUrl();
@@ -151,27 +219,122 @@ export class AccessRootUrl implements IController<Request> {
 		this.eventManager.notify({
 			eventName: EventNames.info,
 			message: {
-				where: 'AccessRootUrl',
-				what: `Iniciando atualização da camada de cache`,
+				where: 'AccessRootUrl.updateCache',
+				what: `Iniciando atualização da camada de cache. Código ${code} e Url: ${rootUrl}.`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.updateCache',
+				what: `Iniciando remoção do chave atual. Chave ${code}.`,
 			},
 		});
 		await this.cache.del(code);
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.updateCache',
+				what: `Chave removida com sucesso. Chave ${code}`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.updateCache',
+				what: `Iniciando inserção da url original na camada de cache. Código ${code} e Url: ${rootUrl}.`,
+			},
+		});
 		await this.cache.set(code, rootUrl);
 		this.eventManager.notify({
 			eventName: EventNames.info,
 			message: {
-				where: 'AccessRootUrl',
-				what: `Camada de cache atualizada`,
+				where: 'AccessRootUrl.updateCache',
+				what: `Url original inserida com sucesso na camada de cache. Código ${code} e Url: ${rootUrl}.`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.updateCache',
+				what: `Camada de cache atualizada com sucesso.`,
 			},
 		});
 	}
 
 	private async getRootUrl(code: string): Promise<string | null> {
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.getRootUrl',
+				what: `Iniciando busca da url original na camada de cache.`,
+			},
+		});
 		const rootUrlCache = await this.getRootUrlFromCache(code);
-		if (rootUrlCache) return rootUrlCache;
+		if (rootUrlCache) {
+			this.eventManager.notify({
+				eventName: EventNames.info,
+				message: {
+					where: 'AccessRootUrl.getRootUrl',
+					what: `Retornando url original encontrada na camada de cache. Url: ${rootUrlCache}`,
+				},
+			});
+			return rootUrlCache;
+		}
+		this.eventManager.notify({
+			eventName: EventNames.warn,
+			message: {
+				where: 'AccessRootUrl.getRootUrl',
+				what: `Url original não encontrada na camada de cache.`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.getRootUrl',
+				what: `Iniciando busca da url original na camada de banco de dados utilizando o código: ${code}`,
+			},
+		});
 		const rootUrlDB = await this.getRootUrlFromDB(code);
-		if (!rootUrlDB) return null;
+		if (!rootUrlDB) {
+			this.eventManager.notify({
+				eventName: EventNames.warn,
+				message: {
+					where: 'AccessRootUrl.getRootUrl',
+					what: `Url original não encontrada na camada de banco de dados utilizando o código: ${code}.`,
+				},
+			});
+			return null;
+		}
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.getRootUrl',
+				what: `Url original encontrada na camada de banco de dados utilizando o código: ${code}.`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.getRootUrl',
+				what: `Iniciando atualização da camada de cache. Code: ${code} Url: ${rootUrlDB}.`,
+			},
+		});
 		await this.updateCache(code, rootUrlDB);
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.getRootUrl',
+				what: `Camada de cache atualizada com sucesso. Code: ${code} Url: ${rootUrlDB}.`,
+			},
+		});
+		this.eventManager.notify({
+			eventName: EventNames.info,
+			message: {
+				where: 'AccessRootUrl.getRootUrl',
+				what: `Retornando url original: ${rootUrlDB} encontrada na camada de banco de dados utilizando o código: ${code}.`,
+			},
+		});
 		return rootUrlDB;
 	}
 
@@ -181,16 +344,16 @@ export class AccessRootUrl implements IController<Request> {
 		this.eventManager.notify({
 			eventName: EventNames.info,
 			message: {
-				where: 'AccessRootUrl',
-				what: 'Enviando dados para a fila de atualização de hits da url encurtada.',
+				where: 'AccessRootUrl.sendToShortenedUrlHitsUpdatedQueue',
+				what: 'Iniciando envio de dados para a fila de atualização de hits da url encurtada.',
 			},
 		});
 		await this.queue.add(QueueName.ShortenedUrlHitsUpdated, data);
 		this.eventManager.notify({
 			eventName: EventNames.info,
 			message: {
-				where: 'AccessRootUrl',
-				what: 'Dados enviados para a fila de atualização de hits da url encurtada.',
+				where: 'AccessRootUrl.sendToShortenedUrlHitsUpdatedQueue',
+				what: 'Envio para a fila de atualização de hits da url encurtada feita com sucesso.',
 			},
 		});
 	}
